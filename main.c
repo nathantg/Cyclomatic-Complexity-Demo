c85f331fe98848795cc8d9d4a8cf3d0932adc89c
nathantg <nathantangca@outlook.com>
2022-12-21 11:11:27 -0500

    first commit

---
+++ main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void) {
+    int i = 0;
+
+    if(i == 1) {
+        i = 100;
+    }
+    else if (i == 2) {
+        i = 100;
+    }
+    else {
+        i = 100;
+    }
+
+    return 1;
+}
